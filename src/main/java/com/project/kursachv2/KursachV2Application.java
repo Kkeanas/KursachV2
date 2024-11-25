7f1c870038ac4dc31ea3decbca374b41f5413977
Maksim Antoshkin <dre.maks474@mail.ru>
2024-11-25 12:46:12 +0300

    Add all files

---
+++ src/main/java/com/project/kursachv2/KursachV2Application.java
@@ -0,0 +1,13 @@
+package com.project.kursachv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursachV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursachV2Application.class, args);
+    }
+
+}
